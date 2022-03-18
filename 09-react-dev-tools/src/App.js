aa900eee0db218a54b255c2fd0223f6f43f28a64
Fabio Coelho <fbclh@protonmail.com>
2022-03-18 10:05:53 -0300

    Finish part 9 - react dev tools

--- 09-react-dev-tools/src/App.js
+++ 09-react-dev-tools/src/App.js
@@ -1,36 +1,17 @@
 // import React from "react"; (No need to import React from React anymore!)q
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt me"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doink",
-//       animal: "Cat",
-//       breed: "Mix",
-//     }),
-//   ]);
-// };
-
 render(<App />, document.getElementById("root"));
-// render(React.createElement(App), document.getElementById("root"));
